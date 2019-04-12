2cffa05a3c89fc429ede4d393530d2e47fc3d848
jihyunjeongme <jihyunjeong.me@gmail.com>
2019-04-12 18:30:32 +0900

    app.py coding

--- app.py
+++ app.py
@@ -82,6 +82,31 @@ def create_app(test_config=None):
 
         return jsonify(created_user)
 
+    @app.route("/tweet", methods=["POST"])
+    def tweet():
+        user_tweet = request.json
+        tweet = user_tweet["tweet"]
+
+        if len(tweet) > 300:
+            return "300자를 초과 했습니다", 400
+
+        app.database.execute(
+            text(
+                """
+            INSERT INTO tweets (
+                user_id,
+                tweet
+            ) VALUES (
+                :id,
+                :tweet
+            )
+        """
+            ),
+            user_tweet,
+        )
+
+        return "", 200
+
     return app
 
 
